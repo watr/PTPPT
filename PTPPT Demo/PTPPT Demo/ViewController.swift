e3f0f481c767d22c25bd05b015bcd8550f3ddfaa
HASHIMOTO Wataru <watr@users.noreply.github.com>
2016-09-11 12:07:47 +0900

    Add demo app.

---
+++ PTPPT Demo/PTPPT Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PTPPT Demo
+//
+//  Created by HASHIMOTO Wataru on 9/11/16.
+//  Copyright © 2016 HASHIMOTO Wataru. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
